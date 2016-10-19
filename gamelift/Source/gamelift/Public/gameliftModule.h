0874fe6a0b6211a899097d09636b774d3ff43cd0
Nam hyeonuk <jwvg0425@naver.com>
2016-10-19 12:00:39 +0900

    add include

--- gamelift/Source/gamelift/Public/gameliftModule.h
+++ gamelift/Source/gamelift/Public/gameliftModule.h
@@ -5,6 +5,7 @@
 #include "aws/gamelift/GameliftClient.h"
 #include "aws/gamelift/model/SearchGameSessionsRequest.h"
 #include "aws/gamelift/model/CreatePlayerSessionRequest.h"
+#include "aws/gamelift/model/CreateGameSessionRequest.h"
 
 
 class GameliftModuleImpl : public IModuleInterface
